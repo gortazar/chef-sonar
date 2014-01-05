b01f0d3cf3d583b5ea4161ff99bcead0c691082a
patxi <patxi.gortazar@gmail.com>
2014-01-05 16:09:26 +0100

    Test improved to check if able to perform a project analysis

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# Test sonar connection
-curl http://localhost:9000
+git clone https://github.com/SonarSource/sonar-examples.git
+
+cd sonar-examples/projects/languages/java/sonar-runner/java-sonar-runner-simple
+
+/opt/sonar-runner/bin/sonar-runner
+
+# Test sonar project connection
+curl http://localhost:9000/java-sonar-runner-simple
 
