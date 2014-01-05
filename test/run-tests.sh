4b77c9ef167217d9665d6223a456c97c226c25b2
patxi <patxi.gortazar@gmail.com>
2014-01-05 16:45:07 +0100

    Retrieving intermediate exit codes to detect failures of each command

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,6 +6,17 @@ cd sonar-examples/projects/languages/java/sonar-runner/java-sonar-runner-simple
 
 /opt/sonar-runner/bin/sonar-runner
 
+if [ 0 -ne $? ]; then
+  echo "Sonar-runner exited with error: $?"
+  exit 1
+fi
+
 # Test sonar project connection
 curl http://localhost:9000/java-sonar-runner-simple
+
+if [ 0 -ne $? ]; then
+  echo "Curl exited with error: $?"
+  exit 1
+fi
+
 
