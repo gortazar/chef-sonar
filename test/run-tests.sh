30b8579d6300fd8881eb7fea0f62d1599c18fc0d
patxi <patxi.gortazar@gmail.com>
2014-01-07 00:05:29 +0100

    Double quotes to escape jdbc url

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,7 +4,8 @@ git clone https://github.com/SonarSource/sonar-examples.git
 
 cd sonar-examples/projects/languages/java/sonar-runner/java-sonar-runner-simple
 
-/opt/sonar-runner/bin/sonar-runner
+/opt/sonar-runner/bin/sonar-runner -Dsonar.jdbc.url="jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" -Dsonar.jdbc.username
+=travis -Dsonar.jdbc.password=
 
 if [ 0 -ne $? ]; then
   echo "Sonar-runner exited with error: $?"
