6aedf0843c1af16ab16494c4fc77a1f31d87956b
patxi <patxi.gortazar@gmail.com>
2014-01-07 01:08:34 +0100

    Fixed newline error

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,8 +4,7 @@ git clone https://github.com/SonarSource/sonar-examples.git
 
 cd sonar-examples/projects/languages/java/sonar-runner/java-sonar-runner-simple
 
-/opt/sonar-runner/bin/sonar-runner -Dsonar.jdbc.url="jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" -Dsonar.jdbc.username
-=travis -Dsonar.jdbc.password=
+/opt/sonar-runner/bin/sonar-runner -Dsonar.jdbc.url="jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" -Dsonar.jdbc.username=travis -Dsonar.jdbc.password=
 
 if [ 0 -ne $? ]; then
   echo "Sonar-runner exited with error: $?"
